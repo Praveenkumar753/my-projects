6b965a8cc09c62f1798f5ed188189587a7741cc7
Praveenkumar753 <100196048+Praveenkumar753@users.noreply.github.com>
2023-07-20 22:00:54 +0530

    Add files via upload
    data_visualizer

--- app.py
+++ app.py
@@ -1,21 +1,60 @@
-from flask import Flask,render_template,request
-app=Flask(__name__)
-@app.route("/",methods=["POST","GET"])
-def calculate():
-    bmi=""
-    if request.method=="POST" and "weight" in request.form and "height" in request.form:
-        weight=float(request.form.get("weight"))
-        height=float(request.form.get("height"))
-        bmi=round(weight/((height/100)**2),2)
-        if float(bmi) < 18.5:
-            bmi = f"You are underweight ({bmi}),so it's better to consume foods that are high in calories and protein to help them gain weight in a healthy way."
-        elif 18.5 <= float(bmi) < 25:
-            bmi = f"Congratulations you have a healthy weight ({bmi}).For better future keep the same diet and take diet with balanced nutritions.  "
-        elif 25 <= float(bmi) < 30:
-            bmi = f"You are overweight ({bmi}),so you have to take balanced diet to support weight loss, regular physical activity is important. Aim for at least 150 minutes of moderate-intensity exercise per week, or as advised by a healthcare professional."
-        else:
-            bmi = f"You have obesity ({bmi}),For obesity, a combination of a healthy diet, regular physical activity, and potentially medical interventions, such as weight loss medications or bariatric surgery, may be necessary. It is important to consult a healthcare professional for personalized advice and support in managing obesity."
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+from flask import Flask, render_template, request, redirect, url_for, flash
 
-    return render_template("index.html",bmi=bmi)
-if __name__=='__main__':
+app = Flask(__name__)
+app.secret_key = "your_secret_key"  # Replace with your own secret key
+
+# Function to visualize the data
+def visualize_data(data, x_column, y_column):
+    x_label = data.columns[x_column]
+    y_label = data.columns[y_column]
+
+    plt.figure(figsize=(10, 6))
+    plt.bar(data[x_label], data[y_label])
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.title("Data Visualization")
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.savefig("static/plot.png")  # Save the plot to a static folder
+    plt.close()
+
+# Flask route for the home page
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        if "file" not in request.files:
+            flash("No file part")
+            return redirect(request.url)
+
+        file = request.files["file"]
+
+        if file.filename == "":
+            flash("No selected file")
+            return redirect(request.url)
+
+        if file and file.filename.endswith(".csv"):
+            file.save("data.csv")  # Save the uploaded CSV file
+            try:
+                data = pd.read_csv("data.csv")
+            except pd.errors.EmptyDataError:
+                flash("The CSV file is empty. Please upload a valid file.")
+                return redirect(url_for("index"))
+
+            if len(data) == 0:
+                flash("The CSV file does not contain any data. Please upload a valid file.")
+                return redirect(url_for("index"))
+
+            x_column = int(request.form["x_column"])  # Get user-inputted x-column number
+            y_column = int(request.form["y_column"])  # Get user-inputted y-column number
+
+            visualize_data(data, x_column, y_column)  # Call the function to visualize the data
+            flash("File uploaded and data visualized successfully!")
+            return redirect(url_for("index"))
+
+    return render_template("index.html", os=os)
+
+if __name__ == "__main__":
     app.run(debug=True)
